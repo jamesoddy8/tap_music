546b0cf7a7c9a57a377381345846f20b3ac168e5
James Oddy <jamesoddy8@gmail.com>
2020-05-13 23:47:08 +0100

    update

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var http = require('http');
+var fs = require('fs');
+
+var html = fs.readFileSync('index.html');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+}).listen(process.env.PORT || 5000);
+
+
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
